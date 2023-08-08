1c56ad5ba8d447a260a9f4c2416d66ce23f91931
sarahrdickerson <sarahrdickerson@gmail.com>
2023-08-08 13:08:00 -0500

    landing page refactored

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main className="flex min-h-screen w-full flex-col items-center justify-center z-30">
+            {children}
+        </main>
+      </body>
     </html>
   )
 }
